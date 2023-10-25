e83c100fda4fb120eb39ebc81e11f4bf890c258c
Connor Adams <connorvadams@gmail.com>
2023-10-24 20:34:43 -0600

    Submits lab.

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(f'{dwarves.index(dwarf) + 1}. {dwarf}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    return [f'{item.title()}!' for item in list]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list):
+    for item in list:
+        if len(item) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    for item in list:
+        if item == 'cheddar':
+            return item
+        
